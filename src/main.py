93ad5434e48486af4527665f1eb4dcb2c040cad8
Santiago Loane <santiago.loane@rochester.edu>
2021-10-25 12:53:30 -0400

    More imports and fixes
    - Cleaned up sample metadata import
    - Added BTFR data import from Lelli 2019

--- src/main.py
+++ src/main.py
@@ -34,7 +34,7 @@ def debug(gal_ID):
 
 # imports the Newtonian Mass Model data from the locally saved copy of the SPARC dataset
 # optimized for (Lelli et al, 2016)
-def import_massmodel_data(filepath='../sparc/MassModels_Lelli2016c.mrt', num_header_rows=25, num_rows=3391):
+def import_massmodel_data(filepath='../sparc/MassModels_Lelli2016c.mrt', num_header_rows=25, num_rows=3416):
 	mm_dt = np.dtype([('ID',		    'U255'),	#	    ID 	: Galaxy Identifier
 					  ('D',			np.float64),	#	     D	: Assumed distance 					(Mpc)
 					  ('R',			np.float64),	#	     R	: Galactocentric radius 			(kpc)
@@ -45,7 +45,7 @@ def import_massmodel_data(filepath='../sparc/MassModels_Lelli2016c.mrt', num_hea
 					  ('Vbul',		np.float64),	#	  Vbul	: Bulge velocity contribution 		(km/s)
 					  ('SBdisk',	np.float64),	#	SBdisk	: Disk surface brightness 			(solLum/pc^2)
 					  ('SBbul',		np.float64)])	#	 SBbul	: Bulge surface brightness 			(solLum/pc^2)
-	mm_data = np.empty((num_rows,), dtype=mm_dt)
+	mm_data = np.empty((num_rows-num_header_rows,), dtype=mm_dt)
 
 	i = 0
 	h_len = num_header_rows	# number of header lines to skip in the file
@@ -73,25 +73,27 @@ def import_massmodel_data(filepath='../sparc/MassModels_Lelli2016c.mrt', num_hea
 def import_galaxy_sample_data(filepath='../sparc/SPARC_Lelli2016c.mrt', num_header_rows=41, data_start_row=98):
 	citations = {}
 	sample_data = {}
-	meta_dt = np.dtype([('ID',			'U255'),		#	    ID 	: Galaxy Identifier
-						('T',			np.int32),		#	     T	: Hubble Type 						hubble_types[T]
-						('D',			np.float64),	#	     D	: Assumed distance 					(Mpc)
-						('e_D',			np.float64),	#	   e_D	: Mean error in D 					(Mpc)
-						('f_D',			np.int32),		#	   f_D	: Distance Method 					dist_methods[f_D]
-						('Inc',			np.float64),	#	   Inc	: Inclination 						(degrees)
-						('e_Inc',		np.float64),	#	 e_Inc	: Mean error in Inclination 		(degrees)
-						('L',			np.float64),	#	     L	: Total Luminosity at [3.6]			(10+9solLum)
-						('e_L',			np.float64),	#	   e_L	: Mean error in Luminosity			(10+9solLum)
-						('Reff',		np.float64),	#	  Reff	: Effective Radius at [3.6] 		(kpc)
-						('SBeff',		np.float64),	#	 SBeff	: Eff. Surface Brightness at [3.6] 	(solLum/pc2)
-						('Rdisk',		np.float64),	#	 Rdisk	: Disk scale length at [3.6] 		(kpc)
-						('SBdisk',		np.float64),	#	SBdisk	: Disk Surface Brightness at [3.6] 	(solLum/pc2)
-						('MHI',			np.float64),	#	   MHI	: Total HI Mass 					(10+9solMass)
-						('RHI',			np.float64),	#	   RHI	: HI Radius at 1 solMass/pc2 		(kpc)
-						('Vflat',		np.float64),	#	 Vflat	: Asymptotic flat rotation vel.		(km/s)
-						('e_Vflat',		np.float64),	#  e_Vflat	: Mean error in Vflat 				(km/s)
-						('Q',			np.int32),		#	     Q	: Quality flag 						quality_flag[T]
-						('Ref',			'U255')])		#	   Ref	: Reference citation				citations[Ref]
+	num_features = 19
+	meta_dt = np.dtype([#('ID',			'U255'),		#	    ID 	: Galaxy Identifier
+					  (  'T',			np.int32),		#	     T	: Hubble Type 						hubble_types[T]
+					  (  'D',			np.float64),	#	     D	: Assumed distance 					(Mpc)
+					  ('e_D',			np.float64),	#	   e_D	: Mean error in D 					(Mpc)
+					  ('f_D',			np.int32),		#	   f_D	: Distance Method 					dist_methods[f_D]
+					  (  'Inc',			np.float64),	#	   Inc	: Inclination 						(degrees)
+					  ('e_Inc',			np.float64),	#	 e_Inc	: Mean error in Inclination 		(degrees)
+					  (  'L',			np.float64),	#	     L	: Total Luminosity at [3.6]			(10+9solLum)
+					  ('e_L',			np.float64),	#	   e_L	: Mean error in Luminosity			(10+9solLum)
+					  (  'Reff',		np.float64),	#	  Reff	: Effective Radius at [3.6] 		(kpc)
+					  (  'SBeff',		np.float64),	#	 SBeff	: Eff. Surface Brightness at [3.6] 	(solLum/pc2)
+					  (  'Rdisk',		np.float64),	#	 Rdisk	: Disk scale length at [3.6] 		(kpc)
+					  (  'SBdisk',		np.float64),	#	SBdisk	: Disk Surface Brightness at [3.6] 	(solLum/pc2)
+					  (  'MHI',			np.float64),	#	   MHI	: Total HI Mass 					(10+9solMass)
+					  (  'RHI',			np.float64),	#	   RHI	: HI Radius at 1 solMass/pc2 		(kpc)
+					  (  'Vflat',		np.float64),	#	 Vflat	: Asymptotic flat rotation vel.		(km/s)
+					  ('e_Vflat',		np.float64),	#  e_Vflat	: Mean error in Vflat 				(km/s)
+					  (  'Q',			np.int32),		#	     Q	: Quality flag 						quality_flag[T]
+					  (  'Refs',		list)])			#	   Refs	: Reference citation(s)				
+					  #('Ref',			'U255')])		#	   Ref	: Reference citation(s)				citations[Ref]
 
 	i = 0
 	for line in open(filepath):
@@ -109,16 +111,52 @@ def import_galaxy_sample_data(filepath='../sparc/SPARC_Lelli2016c.mrt', num_head
 				line_data = line_data[1:]
 			if line_data[-1] == '':
 				line_data = line_data[:-1]
-			# create a numpy array with labeled and type-checked fields
-			line_arr = np.empty((1,), dtype=meta_dt)
-			for dtype_field,val in zip(meta_dt.fields.items(), line_data):
-				dtype_label = dtype_field[0]
-				line_arr[dtype_label] = val
 
-			sample_data[line_arr[0]['ID']] = line_arr[0]
+			# split multiple citations
+			line_data[-1] = line_data[-1].split(',')
+
+			# create a numpy array with labeled and type-checked fields
+			line_arr = np.array([tuple(line_data[1:])], dtype=meta_dt)
+			# hash under galaxy ID
+			sample_data[line_data[0]] = line_arr[0]
 		i += 1
 
 	return sample_data,citations
+
+def import_btfr_data(filepath='../sparc/BTFR_Lelli2019.mrt', num_header_rows=30, num_rows=183):
+	btfr_data = {}
+	btfr_dt = np.dtype([#(  'ID',		'U225'),		#	  ID    	: Galaxy Identifier
+						(  'log_Mb',	np.float64),	#	  log_Mb	: Log of the baryonic mass		(solMass)
+						('e_log_Mb',	np.float64),	#	e_log_Mb	: Mean error on log_Mb  		(solMass)
+						(  'Inc',		np.float64),	#	  Inc   	: Assumed disk inclination		(degrees)
+						('e_Inc',		np.float64),	#	e_Inc   	: Mean error on Inc     		(degrees)
+						(  'Vf',		np.float64),	#	  Vf    	: Flat rotation velocity		(km/s)
+						('e_Vf',		np.float64),	#	e_Vf    	: Mean error on Vf      		(km/s)
+						(  'V2exp',		np.float64),	#	  V2exp 	: Vel at 2.2 exp scale lengths	(km/s)
+						('e_V2exp',		np.float64),	#	e_V2exp 	: Mean error on V2exp   		(km/s)
+						(  'V2eff',		np.float64),	#	  V2eff 	: Vel at 2 eff radii    		(km/s)
+						('e_V2eff',		np.float64),	#	e_V2eff 	: Mean error on V2eff   		(km/s)
+						(  'Vmax',		np.float64),	#	  Vmax  	: Maximum rotation velocity		(km/s)
+						('e_Vmax',		np.float64),	#	e_Vmax  	: Mean error on Vmax    		(km/s)
+						(  'Wp20',		np.float64),	#	  Wp20  	: Linewidth@20% of HI peak flux	(km/s)
+						('e_Wp20',		np.float64),	#	e_Wp20  	: Mean error on Wp20    		(km/s)
+						(  'Wm50',		np.float64),	#	  Wm50  	: Linewidth@50% of HI mean flux	(km/s)
+						('e_Wm50',		np.float64),	#	e_Wm50  	: Mean error on Wp50    		(km/s)
+						(  'Wm50c',		np.float64),	#	  Wm50c 	: Wm50 after instrum. correct	(km/s)
+						('e_Wm50c',		np.float64)])	#	e_Wm50c 	: Mean error on Wm50c			(km/s)
+
+	i = 0
+	for line in open(filepath):
+		if i >= (num_header_rows + 1):
+			line_data = line.split()
+			
+			# create a numpy array with labeled and type-checked fields
+			line_arr = np.array([tuple(line_data[1:])], dtype=btfr_dt)
+			# hash under galaxy ID
+			btfr_data[line_data[0]] = line_arr[0]
+		i += 1
+
+	return btfr_data
 
 if __name__ == '__main__':
 	main()
